1275bdc59ab2ba18fabee08cf014d2ffcb226b3c
XniceCraft <xnicecraftacc@gmail.com>
2022-08-13 18:34:17 +0700

    Update list pertanyaan!

--- run.py
+++ run.py
@@ -5,14 +5,10 @@ from json import loads as jloads
 import random
 
 app=FastAPI()
-@app.get("/", response_class=PlainTextResponse)
-def main():
-    return "Hi"
-
 @app.get("/kota-kabupaten", response_class=PlainTextResponse)
 def kota():
     return getline("kota-kabupaten.txt",random.randint(1,472)).replace("\n","")
 
 @app.get("/pertanyaan", response_class=JSONResponse)
 def pertanyaan():
-    return jloads(getline("list_pertanyaan.json", random.randint(1,17)).replace("\n",""))
+    return jloads(getline("list_pertanyaan.json", random.randint(1,42)).replace("\n",""))
