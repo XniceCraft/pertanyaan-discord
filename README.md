34a8f652aa897c91152352eff24222783e1ba613
XniceCraft <xnicecraftacc@gmail.com>
2022-08-13 19:30:14 +0700

    Update

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
-# Pertanyaan IPS
+# API Pertanyaan
 Ditujukan untuk bot discord 
 
 ### Web
 https://api-pertanyaan.herokuapp.com/
+
+### Credit
+- <a href="https://github.com/nolimitid/nama-tempat-indonesia">Kota dan Kabupaten</a>
--- run.py
+++ run.py
@@ -1,14 +1,36 @@
-from fastapi import FastAPI
-from fastapi.responses import PlainTextResponse, JSONResponse
+#pylint: disable=line-too-long
 from linecache import getline
 from json import loads as jloads
-import random
+from random import randint
+from fastapi import FastAPI
+from fastapi.responses import JSONResponse
 
 app=FastAPI()
-@app.get("/kota-kabupaten", response_class=PlainTextResponse)
-def kota():
-    return getline("kota-kabupaten.txt",random.randint(1,472)).replace("\n","")
+@app.get("/", response_class=JSONResponse)
+def root() -> dict:
+    '''Mereturn status'''
+    return {"status":"berhasil"}
+
+@app.get("/kota-kabupaten", response_class=JSONResponse)
+def kota() -> dict:
+    '''Memilih Kota/Kabupaten secara acak'''
+    pilih_acak=getline("kota-kabupaten.txt",randint(1,472)).replace("\n","")
+    tersensor=""
+    pilih_acak_huruf=[randint(0,len(pilih_acak)-1) for _ in range(int(len(pilih_acak)*randint(1,30)/100))]
+
+    apakah_spasi=False
+    for nomor_iterasi, isi in enumerate(pilih_acak):
+        if nomor_iterasi > 1 and isi != " " and not apakah_spasi and nomor_iterasi not in pilih_acak_huruf:
+            tersensor+="ˍ "
+        else:
+            if isi == " ":
+                apakah_spasi=True
+            elif apakah_spasi:
+                apakah_spasi=False
+            tersensor+=f"{isi} "
+    return {"hasil": pilih_acak, "tersensor": tersensor}
 
 @app.get("/pertanyaan", response_class=JSONResponse)
-def pertanyaan():
-    return jloads(getline("list_pertanyaan.json", random.randint(1,42)).replace("\n",""))
+def pertanyaan() -> dict:
+    '''Mereturn pertanyaan secara acak'''
+    return jloads(getline("daftar-pertanyaan.json", randint(1,42)).replace("\n",""))
