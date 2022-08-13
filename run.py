bc6c8be2f5a1acbe24130dae7de251af47a82cd5
XniceCraft <xnicecraftacc@gmail.com>
2022-08-13 12:25:08 +0700

    Add files

---
+++ run.py
@@ -0,0 +1,15 @@
+from fastapi import FastAPI
+from fastapi.responses import PlainTextResponse, JSONResponse
+from linecache import getline
+from json import loads as jloads
+import random
+
+app=FastAPI()
+
+@app.get("/kota-kabupaten", response_class=PlainTextResponse)
+def kota() -> str:
+    return getline("kota-kabupaten.txt",random.randint(1,472)).replace("\n","")
+
+@app.get("/pertanyaan", response_class=JSONResponse)
+def pertanyaan() -> dict:
+    return jloads(getline("list_pertanyaan.json", random.randint(1,17)).replace("\n",""))
